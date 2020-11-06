ba323cd2f7d817be16ac0d5b560b56c32df2ef7e
Zydrunas-Sir <73161629+Zydrunas-Sir@users.noreply.github.com>
2020-11-06 09:03:25 +0200

    Separated the task

--- CrudExample.py
+++ CrudExample.py
@@ -7,15 +7,14 @@ def create_table_customer():
     first_name TEXT,
     last_name TEXT,
     age INTEGER,
-    company_id INTEGER,
     FOREIGN KEY (company_id) REFERENCES Companies(id))"""
     with DatabaseContextManager("db") as db:
         db.execute(query)
 
 
-def create_customer(first_name: str, last_name: str, age: int, company_id: int):
-    query = """INSERT INTO Customer(first_name, last_name, age, company_id) VALUES(?,?,?,?)"""
-    parameters = [first_name, last_name, age, company_id]
+def create_customer(first_name: str, last_name: str, age: int):
+    query = """INSERT INTO Customer(first_name, last_name, age, company_id) VALUES(?,?,?)"""
+    parameters = [first_name, last_name, age]
     with DatabaseContextManager("db") as db:
         db.execute(query, parameters)
 
@@ -43,36 +42,3 @@ def delete_customer(last_name: str):
     parameters = [last_name]
     with DatabaseContextManager("db") as db:
         db.execute(query, parameters)
-
-
-def create_table_companies():
-    query = """CREATE TABLE IF NOT EXISTS Companies(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    company_name TEXT,
-    employee_count INTEGER,
-    average_salary FLOAT)"""
-    with DatabaseContextManager("db") as db:
-        db.execute(query)
-
-
-def create_companies(company_name: str, employee_count: int, average_salary: float):
-    query = """INSERT INTO Companies(company_name, employee_count, average_salary) VALUES(?,?,?)"""
-    parameters = [company_name, employee_count, average_salary]
-    with DatabaseContextManager("db") as db:
-        db.execute(query, parameters)
-
-
-def get_customer_companies():
-    query = """SELECT * FROM Customer
-            JOIN Companies
-                ON company_id = Companies.id"""
-    with DatabaseContextManager("db") as db:
-        db.execute(query)
-        for record in db.fetchall():
-            print(record)
-
-
-def drop_table():
-    query = """DROP TABLE Customer"""
-    with DatabaseContextManager("db") as db:
-        db.execute(query)
